c34377f3d452cf9909b38f5beaf1563422211678
caicai <1014934017@qq.com>
2022-02-18 16:35:50 +0800

    version2.0: 异步

--- my-promise.js
+++ my-promise.js
@@ -8,16 +8,21 @@ class MyPromise {
     this.value = undefined;
     this.reason = undefined;
 
+    this.onResolveCbs = [];
+    this.onRejectCbs = [];
+
     const resolve = (value) => {
       if (this.status === PENDDING) {
         this.status = FULFILLED;
         this.value = value;
+        this.onResolveCbs.map((fn) => fn(value));
       }
     };
     const reject = (reason) => {
       if (this.status === PENDDING) {
         this.status = REJECTED;
         this.reason = reason;
+        this.onRejectCbs.map((fn) => fn(reason));
       }
     };
     try {
@@ -33,10 +38,17 @@ class MyPromise {
     if (this.status === REJECTED) {
       onReject(this.reason);
     }
+    if (this.status === PENDDING) {
+      this.onResolveCbs.push(onResolve);
+      this.onRejectCbs.push(onReject);
+    }
   }
 }
 new MyPromise((resolve, reject) => {
-  resolve(666);
+  setTimeout(() => {
+    // resolve(666);
+    reject(123);
+  }, 2000);
 }).then(
   (res) => {
     console.log("res===", res);
